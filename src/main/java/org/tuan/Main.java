2b11ca8ebca645aa44a1f1c55f8afca9eb2bea85
Tuan Vu Minh <minhtuan3154@gmail.com>
2026-02-10 18:45:55 +0700

    Delete row and table maintenance

--- src/main/java/org/tuan/Main.java
+++ src/main/java/org/tuan/Main.java
@@ -6,24 +6,40 @@ import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.data.GenericRowData;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.iceberg.Schema;
+import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.flink.CatalogLoader;
+import org.apache.iceberg.flink.FlinkWriteOptions;
 import org.apache.iceberg.flink.TableLoader;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.flink.sink.FlinkSink;
+import org.apache.iceberg.types.Types;
+import org.apache.iceberg.flink.maintenance.api.JdbcLockFactory;
+import org.apache.iceberg.flink.maintenance.api.TableMaintenance;
+import org.apache.iceberg.flink.maintenance.api.TriggerLockFactory;
+import org.apache.iceberg.flink.maintenance.api.ExpireSnapshots;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.time.Duration;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        EnvironmentSettings settings = EnvironmentSettings
+                .newInstance()
+                .inStreamingMode()
+                .build();
+
+        TableEnvironment tableEnv = TableEnvironment.create(settings);
 
         env.enableCheckpointing(10000);
         env.getCheckpointConfig().setMinPauseBetweenCheckpoints(500);
@@ -33,9 +49,8 @@ public class Main {
 
         KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
                 .setBootstrapServers("databus-kafka-bootstrap.kafka.svc:9092")
-                .setGroupId("tuan-test-2")
-                .setTopics("tuan_test_topic_string")
-                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setGroupId("tuan-test-3")
+                .setTopics("flink_topic_test_2")
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
         DataStream<String> dataStream = env.fromSource(
@@ -44,15 +59,15 @@ public class Main {
             "kafka-source"
         );
 
-        String s3Endpoint = "";
+        String s3Endpoint = "http://minio.minio.svc:9000";
         String s3AccessKey = "";
         String s3SecretKey = "";
 
         Map<String, String> catalogOptions = new HashMap<>();
         catalogOptions.put("type", "iceberg");
         catalogOptions.put("catalog-type", "hive");
-        catalogOptions.put("uri", "thrift://datawarehouse-metastore.hive.svc:9083");
-        catalogOptions.put("warehouse", "s3a://datawarehouse/iceberg/.warehouse/");
+        catalogOptions.put("uri", "thrift://-datawarehouse-metastore.hive.svc:9083");
+        catalogOptions.put("warehouse", "s3a://-datawarehouse/iceberg/.warehouse/");
         catalogOptions.put("s3.endpoint", s3Endpoint);
         catalogOptions.put("s3.access-key-id", s3AccessKey);
         catalogOptions.put("s3.secret-access-key", s3SecretKey);
@@ -71,12 +86,48 @@ public class Main {
 //        hadopConf.set("fs.s3a.aws.credentials.provider", "org.apache.flink.fs.s3.common.token.DynamicTemporaryAWSCredentialsProvider");
 
         dataStream.print();
-        CatalogLoader catalogLoader = CatalogLoader.hive("datawarehouse", hadopConf, catalogOptions);
-        TableLoader tableLoader = TableLoader.fromCatalog(catalogLoader, TableIdentifier.of("test_geo", "tuan_dz_str"));
+        CatalogLoader catalogLoader = CatalogLoader.hive("_datawarehouse", hadopConf, catalogOptions);
+
+        Catalog catalog = catalogLoader.loadCatalog();
+        TableIdentifier tableName = TableIdentifier.of("test_geo", "flink_test_upsert");
+        if (!catalog.tableExists(tableName)) {
+            List<Types.NestedField> columns = List.of(
+                Types.NestedField.required(1, "id", Types.StringType.get()),
+                Types.NestedField.optional(2, "name", Types.StringType.get())
+            );
+            Schema schema = new Schema(columns, Set.<Integer>of(1));
+
+            catalog.createTable(tableName, schema);
+        }
+
+
+        TableLoader tableLoader = TableLoader.fromCatalog(catalogLoader, tableName);
         DataStream<RowData> rowDataDataStream = dataStream.map(new RawStringToRowDataMapper());
+
+        Map<String, String> jdbcProps = new HashMap<>();
+        jdbcProps.put("jdbc.user", "");
+        jdbcProps.put("jdbc.password", "");
+
+        TriggerLockFactory lockFactory = new JdbcLockFactory(
+                "jdbc:postgresql://:6432/iceberg",
+                String.format("%s.%s.%s", catalog.name(), tableName.namespace().toString(), tableName.name()),
+                jdbcProps
+        );
+        TableMaintenance.forTable(env, tableLoader, lockFactory)
+                .uidSuffix("production-maintenance")
+                .rateLimit(Duration.ofMinutes(1))
+                .lockCheckDelay(Duration.ofSeconds(30))
+                .parallelism(1)
+                // Daily snapshot cleanup
+                .add(ExpireSnapshots.builder()
+                        .cleanExpiredMetadata(true)
+                        .maxSnapshotAge(Duration.ofMinutes(1))
+                        .retainLast(1))
+                .append();
 
         FlinkSink.forRowData(rowDataDataStream)
                 .tableLoader(tableLoader)
+                .upsert(true)
                 .append();
 
         env.execute("tuan-kafka-app");
--- src/main/java/org/tuan/RawStringToRowDataMapper.java
+++ src/main/java/org/tuan/RawStringToRowDataMapper.java
@@ -4,14 +4,22 @@ import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.table.data.GenericRowData;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.data.StringData;
+import org.apache.flink.types.RowKind;
 import org.apache.iceberg.flink.TableLoader;
 
 public class RawStringToRowDataMapper implements MapFunction<String, RowData> {
     @Override
     public RowData map(String s) throws Exception {
         System.out.println("String value: " + s);
-        GenericRowData rowData = new GenericRowData(1);
-        rowData.setField(0, StringData.fromString(s));
+        String[] parts = s.split(",");
+
+        GenericRowData rowData = new GenericRowData(2);
+        rowData.setField(0, StringData.fromString(parts[0]));
+        rowData.setField(1, StringData.fromString(parts[1]));
+        if (parts.length > 2) {
+            rowData.setRowKind(RowKind.DELETE);
+        }
+
         System.out.println("Row data: " + rowData);
         return rowData;
     }
